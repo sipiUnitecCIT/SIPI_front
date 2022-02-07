1e356d5c008f3321c97ca24cafef2c0a9a4d7bb0
OrlandoMendoza17 <ommv.17@gmail.com>
2022-02-07 19:12:14 -0400

    Se terminó la vista Login Desktop

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -25,7 +25,7 @@ const SignIn = () => {
 
   return (
     <main className="SignIn">
-      <aside className='bg-secondary text-white'>
+      <aside>
         <h1 className='text-lg xx_sm:text-2xl font-bold'>Sistema de Información de Proyectos Institucionales</h1>
         <p className='text-xs xx_sm:text-base font-light'>Aquí encontrarás todo lo relacionado con <span className='font-semibold underline underline-offset-1'>Proyectos Institucionales.</span></p>
         <p className='text-xs xx_sm:text-base font-light'>Es el canal de comunicación entre Proyectos Institucionales y la Universidad Tecnológica del Centro (UNITEC)</p>
@@ -35,11 +35,12 @@ const SignIn = () => {
           <h2>Iniciar Sesión</h2>
           <section>
             <InputForm
-              type="text"
+              type="email"
               title="Usuario ID"
               id="userName"
               handleChange={handleChange}
               value={userName}
+              required
             />
             <InputForm
               type="password"
@@ -47,6 +48,7 @@ const SignIn = () => {
               id="password"
               handleChange={handleChange}
               value={password}
+              required
             />
             <InputCheck
               title="Recordarme en el Sistema"
@@ -54,7 +56,7 @@ const SignIn = () => {
             <Button type="submit">
               Ingresar
             </Button>
-            <Anchor className="font-semibold text-sm text-neutral-60">
+            <Anchor className="font-semibold text-sm text-neutral-60" href="/">
               Recuperar Contraseña
             </Anchor>
           </section>
